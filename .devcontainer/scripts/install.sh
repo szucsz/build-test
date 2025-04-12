5b97df7f0a62147b4dabf1634d36b709b40de6d9
Zoltan Szucs <zz.szucs@gmail.com>
2025-04-12 17:27:38 +0000

    switch to ubuntu

---
+++ .devcontainer/scripts/install.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt install -y \
+    bash \
+    make \
+    gcc \
+    bison \
+    flex \
+    python3-dev \
+    python3-setuptools \
+    swig \
+    libssl-dev \
+    bc \
+    fdisk \
+    kmod \
+    gcc-aarch64-linux-gnu \
+    libgnutls28-dev \
+    build-essential \
+    device-tree-compiler
