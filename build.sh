9c4d7b0d054368e043e95cbcc00b4ebaaa1d6675
Zoltan Szucs <zz.szucs@gmail.com>
2025-04-12 19:52:54 +0200

    remove linux

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-git clone --depth 1 --branch master https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
+#!/bin/bash
+
 cd linux
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
 time make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j7 Image dtbs modules
